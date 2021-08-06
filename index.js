fd0ec8d4ecdeeda997c2c73ab467adfa4d29a2cf
neverdiephil <76856380+neverdiephil@users.noreply.github.com>
2021-08-06 00:02:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-function getRandomSize(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-  }
-  
-
-  var run = () => {
-    document.querySelector('#photos').style.opacity = 0;
-    var allImages = "";
-  
-    for (var i = 0; i < 25; i++) {
-      var width = getRandomSize(200, 400);
-      var height =  getRandomSize(200, 400);
-      allImages += '<li><img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty"></li>';
+var gallery = document.querySelector('#gallery');
+var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
+var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
+var resizeAll = function () {
+    var altura = getVal(gallery, 'grid-auto-rows');
+    var gap = getVal(gallery, 'grid-row-gap');
+    gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+        var el = item;
+        el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+    });
+};
+gallery.querySelectorAll('img').forEach(function (item) {
+    item.classList.add('byebye');
+    if (item.complete) {
+        console.log(item.src);
     }
-    document.querySelector('#photos').append(allImages);
-    document.querySelector('#photos').style.opacity = 1;
-  }
-  run;
+    else {
+        item.addEventListener('load', function () {
+            var altura = getVal(gallery, 'grid-auto-rows');
+            var gap = getVal(gallery, 'grid-row-gap');
+            var gitem = item.parentElement.parentElement;
+            gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+            item.classList.remove('byebye');
+        });
+    }
+});
+window.addEventListener('resize', resizeAll);
+gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+    item.addEventListener('click', function () {        
+        item.classList.toggle('full');        
+    });
+});
